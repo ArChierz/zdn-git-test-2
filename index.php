9567ab33eb41508617d5ad17d9729f3b29341c94
ArChierz <zidan.ramadhan102@gmail.com>
2023-04-20 20:10:46 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba GIT Remote</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, unde! Aut nihil voluptate aliquid dicta ipsa asperiores illo quisquam soluta distinctio nesciunt adipisci, necessitatibus tempore quaerat, laboriosam, veniam quo voluptates?</p>
+    
+</body>
+</html>
